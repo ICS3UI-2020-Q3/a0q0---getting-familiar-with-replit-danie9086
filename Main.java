7814cb4d2c416dac00384721a8a807a8434a0006
Evan Daniel <danie9086@wrdsb.ca>
2021-02-18 16:51:19 +0000

    Name and age program

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 /**
- * The hello world program
+ * Name and age program
  * @author Evan Daniel
  */
 
@@ -10,7 +12,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("Hello World!");
-    
+    // ask the user for their name
+    System.out.println("Please enter your name:");
+
+    // creating a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // declare and initialize the name variable
+    String name = input.nextLine();
+    // say hello to the user
+    System.out.println("Hello " + name);
+
+    // ask the user when they were born
+    System.out.println("Please enter the year you were born:");
+
+    // create the variable to store the birth year
+    int birthYear = input.nextInt();
+
+    // calculate the approximate age of the user
+    final int CURRENT_YEAR = 2021;
+    int age = CURRENT_YEAR - birthYear;
+
+    // tell the user their age
+    System.out.println("You are approximately " + age + " years old.");
+
   }
+
 }
